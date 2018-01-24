6e78ecc786970f11d3ae6b33abaaa3f657893200
yangshunfan <15700734908@163.com>
2018-01-24 15:27:10 +0800

    组合模式

---
+++ src/com/composite/pattern/Employee.java
@@ -0,0 +1,40 @@
+package com.composite.pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create by yangshunfan
+ * 2018/1/24 15:11
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+}
